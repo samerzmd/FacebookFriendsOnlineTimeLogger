9e4fa1a3ad36b8d0ddfc55bed84df94e1c4ad662
Sam.Z.D <samerzmd@gmail.com>
2016-05-01 23:18:50 +0300

    initiate

---
+++ FacebookFriendsOnlineTimeLogger/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookFriendsOnlineTimeLogger.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public List<DateTime> DateTimes { get; set; }
+
+    }
+}
